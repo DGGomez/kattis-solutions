b749e59c2fb8e49fbc409193088d8615953ddb05
DGGomez <dggomez21@gmail.com>
2016-12-27 17:37:55 -0500

    Update different.java

--- kattis/different.java
+++ kattis/different.java
@@ -1,22 +1,33 @@
 package kattis;
-
+/**
+ * Solution for Kattis problem-id: A different problem
+ * https://open.kattis.com/problems/different
+ *
+ * Author: Daniel Gomez
+ */
 import java.util.Scanner;
 import java.lang.Math;
 
-public class different {
-public static void main(String[]args){
+public class different 
+{
+public static void main(String[]args)
+{
 	long a;
 	long b;
 	long result;
 	Scanner scanner = new Scanner(System.in);
-	while(scanner.hasNextLong()){
+	while(scanner.hasNextLong())
+	{
 	a= scanner.nextLong();
 	b=scanner.nextLong();
-	if(a>b){
+		
+	if(a>b)
+	{
 		result= Math.abs(a-b);
 		System.out.println(result);
 	}
-	else{
+	else
+	{
 		result=Math.abs(b-a);
 		System.out.println(result);
 	}}
